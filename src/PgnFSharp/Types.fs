8ab0620e0dc54a5f7a444f30720a4e8e19b7c360
pbbwfc <phil.brooks@asr-services.com>
2016-03-24 18:49:59 +0000

    Perf fix

--- src/PgnFSharp/Types.fs
+++ src/PgnFSharp/Types.fs
@@ -190,27 +190,6 @@ type Posn =
     
     /// Gets Move from string
     member x.GetMv mv = 
-        ///Dictionary of files
-        let fDct = 
-            [ 'a'..'h' ]
-            |> List.mapi (fun i c -> c, i)
-            |> dict
-        
-        ///Dictionary of ranks
-        let rDct = 
-            [ 1..8 ]
-            |> List.rev
-            |> List.mapi (fun i c -> char (c.ToString()), i)
-            |> dict
-        
-        ///Dictionary of squares
-        let SqDct = 
-            [ for r = 8 downto 1 do
-                  for f in [ 'a'..'h' ] do
-                      yield f.ToString() + r.ToString() ]
-            |> List.mapi (fun i s -> s, i)
-            |> dict
-        
         //general failure message
         let fl() = failwith ("not done yet, mv: " + mv + " pos: " + x.ToString())
         let samef a b = a % 8 = b % 8
---
+++ src/PgnFSharp/Utils.fs
@@ -0,0 +1,24 @@
+﻿namespace PgnFSharp
+
+[<AutoOpen>]
+module Utils = 
+    ///Dictionary of files
+    let fDct = 
+        [ 'a'..'h' ]
+        |> List.mapi (fun i c -> c, i)
+        |> dict
+    
+    ///Dictionary of ranks
+    let rDct = 
+        [ 1..8 ]
+        |> List.rev
+        |> List.mapi (fun i c -> char (c.ToString()), i)
+        |> dict
+    
+    ///Dictionary of squares
+    let SqDct = 
+        [ for r = 8 downto 1 do
+              for f in [ 'a'..'h' ] do
+                  yield f.ToString() + r.ToString() ]
+        |> List.mapi (fun i s -> s, i)
+        |> dict
